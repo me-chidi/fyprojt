45c4745e294ec9d0bb0646a5f2a63053611e0fa8
CJ21 <jconuekwusi@gmail.com>
2025-04-30 16:25:07 +0100

    changed relative path to abspath for SQL_URI

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "instance", "database.db")
 SECRET_KEY = os.environ.get("FSKY") or "not-a-secret"
 MAIN_PROC = os.environ.get("WERKZEUG_RUN_MAIN") != "true"
 SERVER_NAME = "127.0.0.1" or "localhost"
